489e78f303f4fc2f3d5def4505b7d338b2703353
Sven Ullmann <kontakt@sumedia-webdesign.de>
2019-12-09 00:27:53 +0100

    avoid constant will be loaded twice

--- sumedia-base.php
+++ sumedia-base.php
@@ -56,8 +56,12 @@ if (-1 == version_compare(PHP_VERSION, '5.6.0')) {
 } else {
 
     define('SUMEDIA_BASE_VERSION', '0.1.1');
-    define('SUMEDIA_PLUGIN_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-    define('SUMEDIA_PLUGIN_URL', plugin_dir_url(__DIR__));
+    if (!defined('SUMEDIA_PLUGIN_PATH')) {
+        define('SUMEDIA_PLUGIN_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+    }
+    if (!defined('SUMEDIA_PLUGIN_PATH')) {
+        define('SUMEDIA_PLUGIN_URL', plugin_dir_url(__DIR__));
+    }
     define('SUMEDIA_BASE_PLUGIN_NAME', dirname(plugin_basename(__FILE__)));
 
     require_once(__DIR__ . str_replace('/', DIRECTORY_SEPARATOR, '/inc/functions.php'));
