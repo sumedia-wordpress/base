2e3ceeff2baaf292ed689f900b0e5ad158d4a636
Sven Ullmann <kontakt@sumedia-webdesign.de>
2019-12-14 17:01:34 +0100

    hmpf

--- admin/view/class-admin-view-heading.php
+++ admin/view/class-admin-view-heading.php
@@ -2,20 +2,117 @@
 
 class Sumedia_Base_Admin_View_Heading extends Sumedia_Base_View
 {
-    public $title;
+    /**
+     * @var string
+     */
+    protected $title;
 
-    public $side_title;
+    /**
+     * @var string
+     */
+    protected $side_title;
 
-    public $version;
+    /**
+     * @var string
+     */
+    protected $version;
 
-    public $iconfile;
+    /**
+     * @var string
+     */
+    protected $iconfile;
+
+    /**
+     * @var array
+     */
+    protected $options = [];
 
     public function __construct()
     {
-        $this->template = Suma\ds(SUMEDIA_BASE_PATH . '/admin/templates/heading.phtml');
-        $this->title = __('Sumedia Plugins', 'sumedia-base');
-        $this->side_title = __('Plugin Overview', 'sumedia-base');
-        $this->version = SUMEDIA_BASE_VERSION;
-        $this->iconfile = SUMEDIA_BASE_URL . '/assets/images/kolibri-logo.png';
+        $this->set_template(Suma\ds(SUMEDIA_BASE_PATH . '/admin/templates/heading.phtml'));
+        $this->set_title(__('Sumedia Plugins', SUMEDIA_BASE_PLUGIN_NAME));
+        $this->set_side_title(__('Plugin Overview', SUMEDIA_BASE_PLUGIN_NAME));
+        $this->set_version(SUMEDIA_BASE_PLUGIN_NAME . ' - ' . SUMEDIA_BASE_VERSION);
+        $this->set_iconfile(SUMEDIA_BASE_URL . '/assets/images/wp-n-suma.png');
+    }
+
+    /**
+     * @return string
+     */
+    public function get_title()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function set_title($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_side_title()
+    {
+        return $this->side_title;
+    }
+
+    /**
+     * @param string $side_title
+     */
+    public function set_side_title($side_title)
+    {
+        $this->side_title = $side_title;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_version()
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param string $version
+     */
+    public function set_version($version)
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_iconfile()
+    {
+        return $this->iconfile;
+    }
+
+    /**
+     * @param string $iconfile
+     */
+    public function set_iconfile($iconfile)
+    {
+        $this->iconfile = $iconfile;
+    }
+
+    /**
+     * @return array
+     */
+    public function get_options()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function set_options($options)
+    {
+        $this->options = $options;
     }
 }
